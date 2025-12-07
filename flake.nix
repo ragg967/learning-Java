8231c9608371bd7d4b076cf4f613d5062b5b8947
Ragg967 <rywatson1027@gmail.com>
2025-12-07 12:39:24 -0800

    fixed it..?

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Project development environment/Project name";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          temurin-bin-21
+          jdt-language-server
+          google-java-format # for formatting
+          checkstyle
+        ];
+
+        shellHook = ''
+          export PROJECT_ROOT=$(pwd)
+          echo "Development environment loaded!"
+        '';
+      };
+    };
+}
