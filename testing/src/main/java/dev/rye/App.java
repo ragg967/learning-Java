82d929ac9fd0d3bff4c6b216e0fe6f3287ff1666
Ragg967 <rywatson1027@gmail.com>
2025-12-07 19:10:38 -0800

    baller

--- testing/src/main/java/dev/rye/App.java
+++ testing/src/main/java/dev/rye/App.java
@@ -1,7 +1,30 @@
 package dev.rye;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    // Weight converter
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter you conversion. e.g. Kg -> lb: ");
+    String conversion = scanner.nextLine();
+
+    if (conversion.toLowerCase().strip().replaceAll(" ", "").contains("kg->lb")) {
+      System.out.print("How many Kilos?: ");
+      double weight = scanner.nextDouble();
+      double converetedWeight = weight * 2.20462;
+      System.out.printf("%.05f kilos is %.05f pounds", weight, converetedWeight);
+    }
+
+    if (conversion.toLowerCase().strip().replaceAll(" ", "").contains("lb->kg")) {
+      System.out.print("How many Pounds?: ");
+      double weight = scanner.nextDouble();
+      double converetedWeight = weight * 0.45359237;
+      System.out.printf("%.05f pounds is %.05f kilos", weight, converetedWeight);
+    }
+
+    scanner.close();
   }
 }
