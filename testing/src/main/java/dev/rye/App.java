86a9f8e9443a6255c3cc61b04ebd15c28b762fd4
Ragg967 <rywatson1027@gmail.com>
2025-12-07 19:20:24 -0800

    added error

--- testing/src/main/java/dev/rye/App.java
+++ testing/src/main/java/dev/rye/App.java
@@ -8,21 +8,29 @@ public class App {
 
     Scanner scanner = new Scanner(System.in);
 
+    boolean hasBeenConverted = false;
+
     System.out.print("Enter you conversion. e.g. Kg -> lb: ");
     String conversion = scanner.nextLine();
 
-    if (conversion.toLowerCase().strip().replaceAll(" ", "").contains("kg->lb")) {
+    if (conversion.toLowerCase().strip().replaceAll("\\s+", "").contains("kg->lb")) {
       System.out.print("How many Kilos?: ");
       double weight = scanner.nextDouble();
-      double converetedWeight = weight * 2.20462;
-      System.out.printf("%.05f kilos is %.05f pounds", weight, converetedWeight);
+      double convertedWeight = weight * 2.20462;
+      System.out.printf("%.05f kilos is %.05f pounds\n", weight, convertedWeight);
+      hasBeenConverted = true;
     }
 
-    if (conversion.toLowerCase().strip().replaceAll(" ", "").contains("lb->kg")) {
+    if (conversion.toLowerCase().strip().replaceAll("\\s+", "").contains("lb->kg")) {
       System.out.print("How many Pounds?: ");
       double weight = scanner.nextDouble();
-      double converetedWeight = weight * 0.45359237;
-      System.out.printf("%.05f pounds is %.05f kilos", weight, converetedWeight);
+      double convertedWeight = weight * 0.45359237;
+      System.out.printf("%.05f pounds is %.05f kilos\n", weight, convertedWeight);
+      hasBeenConverted = true;
+    }
+
+    if(!hasBeenConverted) {
+      System.out.printf("Error: unable to convert %s\n", conversion);
     }
 
     scanner.close();
