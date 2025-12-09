7d59849acfe35f8b88accb5b0579b21c39a6c802
Ragg967 <rywatson1027@gmail.com>
2025-12-09 09:29:01 -0800

    baller

--- testing/src/main/java/dev/rye/App.java
+++ testing/src/main/java/dev/rye/App.java
@@ -1,7 +1,21 @@
 package dev.rye;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Scanner scanner = new Scanner(System.in);
+
+    boolean hasBeenConverted = false;
+
+    System.out.print("Enter your conversion. e.g. F -> C: ");
+    String conversion = scanner.nextLine();
+
+    // Store normalized version to avoid repeating the operation
+    String normalized = conversion.toLowerCase().strip().replaceAll("\\s+", "");
+
+    if(normalized.contains("f->c")){}
+
+    scanner.close();
   }
 }
