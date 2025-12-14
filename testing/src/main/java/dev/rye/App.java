c13c7e1444882b76b709a771b700f243029b7bb9
Ragg967 <rywatson1027@gmail.com>
2025-12-13 21:50:46 -0800

    b

--- testing/src/main/java/dev/rye/App.java
+++ testing/src/main/java/dev/rye/App.java
@@ -4,28 +4,15 @@ import javax.swing.*;
 
 public class App {
   public static void main(String[] args) {
-    // Creating instance of JFrame
     JFrame frame = new JFrame("My Application");
-
-    // Set close operation - IMPORTANT!
+    frame.setLayout(null);
+    frame.setSize(500, 600);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    // Creating instance of JButton
-    JButton button = new JButton(" GFG WebSite Click");
-
-    // x axis, y axis, width, height
+    JButton button = new JButton("Click me");
     button.setBounds(150, 200, 220, 50);
 
-    // adding button in JFrame
     frame.add(button);
-
-    // 400 width and 500 height
-    frame.setSize(500, 600);
-
-    // using no layout managers
-    frame.setLayout(null);
-
-    // making the frame visible
     frame.setVisible(true);
   }
 }
