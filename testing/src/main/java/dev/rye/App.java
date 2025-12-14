68c7971174877c1b52da5466d1ccd4e3d51b54f2
Ragg967 <rywatson1027@gmail.com>
2025-12-13 21:35:41 -0800

    got it working

--- testing/src/main/java/dev/rye/App.java
+++ testing/src/main/java/dev/rye/App.java
@@ -4,14 +4,28 @@ import javax.swing.*;
 
 public class App {
   public static void main(String[] args) {
-    JFrame frame = new JFrame("My App");
+    // Creating instance of JFrame
+    JFrame frame = new JFrame("My Application");
+
+    // Set close operation - IMPORTANT!
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JButton button = new JButton("Click me!");
-    button.addActionListener(e-> System.out.println("Clicked!"));
+    // Creating instance of JButton
+    JButton button = new JButton(" GFG WebSite Click");
 
+    // x axis, y axis, width, height
+    button.setBounds(150, 200, 220, 50);
+
+    // adding button in JFrame
     frame.add(button);
-    frame.pack();
+
+    // 400 width and 500 height
+    frame.setSize(500, 600);
+
+    // using no layout managers
+    frame.setLayout(null);
+
+    // making the frame visible
     frame.setVisible(true);
   }
 }
